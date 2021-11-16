c41f9256567260ff84a6cdea337dbc6f590f3275
Tim Quimbo <tquimbo1@gmail.com>
2021-11-15 23:28:50 -0500

    commit tree 614d86e70c831682838a207fae42ea0252612085

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+let userData = {
+    name: "Steve",
+    email: "steve@steve.com"
+  };
+  
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+  
+  function submitData() {
+    return fetch("http://localhost:3000/users", configObj)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        document.body.innerHTML = object['id'];
+      })
+      .catch(function(error) {
+        alert("Unauthorized Access");
+        document.body.innerHTML = error.message
+      })
+  };
+  
